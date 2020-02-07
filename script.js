ce625aebb541a06c5c15f281302fe010ca0a0f82
“Ronny <contact@ronnyperez.com>
2020-02-07 11:51:35 -0600

    added the google search bar

--- script.js
+++ script.js
@@ -4,66 +4,66 @@ var $nytHeadline = $("#nyt-headline");
 var $nytImage = $("#nyt-image");
 var $nytButton = $("#nyt-button");
 
-
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&fq=news_desk:('Politics')('Washington')&api-key=PRDkjbCFU5eSMenXggU1owKkJyQjyAnU";
+var queryURL =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&fq=news_desk:('Politics')('Washington')&api-key=PRDkjbCFU5eSMenXggU1owKkJyQjyAnU";
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    // console.log(response);
-    $nytHeadline.text(response.response.docs[0].headline.main);
-    $nytImage.attr("src", "https://nytimes.com/" + response.response.docs[0].multimedia[2].url);
-    $nytButton.attr("href", response.response.docs[0].web_url);
-
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  // console.log(response);
+  $nytHeadline.text(response.response.docs[0].headline.main);
+  $nytImage.attr(
+    "src",
+    "https://nytimes.com/" + response.response.docs[0].multimedia[2].url
+  );
+  $nytButton.attr("href", response.response.docs[0].web_url);
 });
 
 // This is the reddit API
 
 var redditSettings = {
-    async: true,
-    crossDomain: true,
-    url: "https://www.reddit.com/r/politics/top.json?count=1",
-    method: "GET",
-    data: {}
-  };
-  
-  $.ajax(redditSettings).done(function(response) {
-    var url = response.data.children[0].data.url; // this is the link for the article
-    var title = response.data.children[0].data.title; //this is title
-  
-    // console.log(response);
-    // console.log(url);
-    // console.log(title);
-    $(".red-title").text(title);
-    $(".red-URL").attr("href", url);
-  });
-  
-  
-  // This is the guardian API
-  
-  var guardianSettings = {
-      // async: true,
-      // crossDomain: true,
-      url: "https://content.guardianapis.com/search?q=us-news%2Fus-elections-2020&api-key=a42a50d2-e130-42f2-98e1-e6e71b872da7&show-fields=thumbnail",
-      method: "GET",
-      // data: {}
-    };
-    
-    $.ajax(guardianSettings).done(function(response) {
-      var tgaUrl = response.response.results[0].webUrl; // this is the link for the article
-      var title = response.response.results[0].webTitle; //this is title
-      var tgaImg = response.response.results[0].fields.thumbnail;
+  async: true,
+  crossDomain: true,
+  url: "https://www.reddit.com/r/politics/top.json?count=1",
+  method: "GET",
+  data: {}
+};
 
-      // console.log("test")
-      // console.log("look at me", response);
-      // console.log("is this the url?", tgaUrl);
-      // console.log("do I see the title?", title);
-      // console.log("do I see the image?", tgaImg);
+$.ajax(redditSettings).done(function(response) {
+  var url = response.data.children[0].data.url; // this is the link for the article
+  var title = response.data.children[0].data.title; //this is title
 
-      $(".tga-title").text(title);
-      $(".tga-URL").attr("href", tgaUrl);
-      $(".tga-IMG").attr("src", tgaImg);
-    });
-  
-    
+  // console.log(response);
+  // console.log(url);
+  // console.log(title);
+  $(".red-title").text(title);
+  $(".red-URL").attr("href", url);
+});
+
+// This is the guardian API
+
+var guardianSettings = {
+  // async: true,
+  // crossDomain: true,
+  url:
+    "https://content.guardianapis.com/search?q=us-news%2Fus-elections-2020&api-key=a42a50d2-e130-42f2-98e1-e6e71b872da7&show-fields=thumbnail",
+  method: "GET"
+  // data: {}
+};
+
+$.ajax(guardianSettings).done(function(response) {
+  var tgaUrl = response.response.results[0].webUrl; // this is the link for the article
+  var title = response.response.results[0].webTitle; //this is title
+  var tgaImg = response.response.results[0].fields.thumbnail;
+
+  // console.log("test")
+  // console.log("look at me", response);
+  // console.log("is this the url?", tgaUrl);
+  // console.log("do I see the title?", title);
+  // console.log("do I see the image?", tgaImg);
+
+  $(".tga-title").text(title);
+  $(".tga-URL").attr("href", tgaUrl);
+  $(".tga-IMG").attr("src", tgaImg);
+});
